5b9f5afc0761498b8f101ca2e1a2e3b4e3671cab
EslamKamel89 <elsamkamelforex@gmail.com>
2026-01-21 23:51:56 +0200

    feat(chat): add message form with crispy forms and handle message creation

---
+++ a_rtchat/forms.py
@@ -0,0 +1,40 @@
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import HTML, Div, Field, Layout, Submit
+from django.forms import ModelForm
+
+from .models import *
+
+
+class GroupMessageCreateForm(ModelForm):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper(self)
+        self.helper.form_method = "POST"
+        self.helper.form_class = "flex items-center gap-2"
+        self.helper.layout = Layout(
+            Div(
+                Field(
+                    "body",
+                    css_class="flex-grow",
+                    placeholder="Add message…",
+                    rows=1,
+                    maxlength="150",
+                ),
+                css_class="flex-grow",
+            ),
+            HTML(
+                """
+        <button type="submit" class="!px-4 !bg-transparent">
+          <i class="bi bi-send-fill text-primary text-2xl"></i>
+        </button>
+                 """
+            ),
+        )
+
+    class Meta:
+        model = GroupMessage
+        fields = ("body",)
+        labels = {
+            "body": "",
+        }
--- a_rtchat/views.py
+++ a_rtchat/views.py
@@ -3,15 +3,37 @@ from django.http import HttpRequest
 from django.shortcuts import get_object_or_404, render
 from django.views import View
 
+from a_rtchat.forms import GroupMessageCreateForm
+
 from .models import *
 
 
 class ChatView(LoginRequiredMixin, View):
-    def get(self, request: HttpRequest):
+    def get_chat_data(self):
         chat_group = get_object_or_404(ChatGroup, group_name="Django")
         chat_messages = chat_group.chat_messages.all()[:30]
+        return (chat_group, chat_messages)
+
+    def get(self, request: HttpRequest):
+        chat_group, chat_messages = self.get_chat_data()
+        form = GroupMessageCreateForm()
         return render(
             request,
             "a_rtchat/chat.html",
-            {"chat_group": chat_group, "chat_messages": chat_messages},
+            {"chat_group": chat_group, "chat_messages": chat_messages, "form": form},
+        )
+
+    def post(self, request: HttpRequest):
+        chat_group, chat_messages = self.get_chat_data()
+        form = GroupMessageCreateForm(data=request.POST)
+        if form.is_valid():
+            message_obj: GroupMessage = form.save(commit=False)
+            message_obj.author = request.user  # type: ignore
+            message_obj.group = chat_group
+            message_obj.save()
+            form.data = {}
+        return render(
+            request,
+            "a_rtchat/chat.html",
+            {"chat_group": chat_group, "chat_messages": chat_messages, "form": form},
         )
