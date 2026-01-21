ec05ba8f7227c679716ad8e3a7f2754fb8497fd6
EslamKamel89 <elsamkamelforex@gmail.com>
2026-01-21 21:41:42 +0200

    feat(chat): add group message model with relations and optimized manager

--- a_rtchat/models.py
+++ a_rtchat/models.py
@@ -1,9 +1,40 @@
+from typing import TYPE_CHECKING
+
+from django.contrib.auth.models import User
 from django.db import models
+
+if TYPE_CHECKING:
+    from .models import GroupMessage
 
 
 class ChatGroup(models.Model):
     id: int
     group_name = models.CharField(max_length=128, unique=True)
+    chat_messages: list[GroupMessage]
 
     def __str__(self) -> str:
         return self.group_name
+
+
+class GroupMessageManager(models.Manager):
+    def get_queryset(self) -> models.QuerySet:
+        return super().get_queryset().select_related("group", "author")
+
+
+class GroupMessage(models.Model):
+    id: int
+    group = models.ForeignKey(
+        ChatGroup, related_name="chat_messages", on_delete=models.CASCADE
+    )
+    author = models.ForeignKey(
+        User, related_name="chat_messages", on_delete=models.CASCADE
+    )
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    objects = GroupMessageManager()
+
+    def __str__(self) -> str:
+        return f"{self.author.username} : {self.body}"
+
+    class Meta:
+        ordering = ("-created_at",)
