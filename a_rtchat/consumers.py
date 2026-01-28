b258bf96455539f1cfe9b4ccb4531be74233b522
EslamKamel89 <elsamkamelforex@gmail.com>
2026-01-28 19:56:14 +0200

    feat(chat): implement real-time presence with online user count via WebSockets

--- a_rtchat/consumers.py
+++ a_rtchat/consumers.py
@@ -2,6 +2,7 @@ import json
 
 from asgiref.sync import async_to_sync
 from channels.generic.websocket import WebsocketConsumer
+from django.contrib.auth.models import User
 
 from a_rtchat.forms import GroupMessageCreateForm
 from a_rtchat.models import ChatGroup, GroupMessage
@@ -11,15 +12,32 @@ from a_rtchat.types import MessageResponse
 class ChatRoomConsumer(WebsocketConsumer):
     def connect(self):
         self.room_group_name = f"chat-{self.get_room_name()}"
+        self.user: User = self.scope.get("user")  # type: ignore
+        if not self.user.is_authenticated:
+            self.close()
+            return
         async_to_sync(self.channel_layer.group_add)(
             self.room_group_name, self.channel_name
         )
+        chat_group: ChatGroup = self.get_chat_group()
+
+        self.chat_group = chat_group
+        if (
+            self.chat_group
+            and not self.chat_group.users_online.filter(id=self.user.id).exists()  # type: ignore
+        ):
+            self.chat_group.users_online.add(self.user)
+            self.update_online_count()
         self.accept()
 
     def disconnect(self, code: int) -> None:
         async_to_sync(self.channel_layer.group_discard)(
             self.room_group_name, self.channel_name
         )
+        chat_group = self.chat_group
+        if chat_group and self.chat_group.users_online.filter(id=self.user.id).exists():  # type: ignore
+            chat_group.users_online.remove(self.user)
+            self.update_online_count()
         return super().disconnect(code)
 
     def receive(
@@ -29,7 +47,7 @@ class ChatRoomConsumer(WebsocketConsumer):
     ) -> None:
         if not text_data:
             return
-        chat_group = self.get_chat_group()
+        chat_group = self.chat_group
         try:
             data = json.loads(text_data)
         except Exception as e:
@@ -46,7 +64,7 @@ class ChatRoomConsumer(WebsocketConsumer):
             )
             return
         message: GroupMessage = form.save(commit=False)
-        message.author = self.scope.get("user")
+        message.author = self.user
         message.group = chat_group
         message.save()
         message_response: MessageResponse = {
@@ -61,17 +79,43 @@ class ChatRoomConsumer(WebsocketConsumer):
         async_to_sync(self.channel_layer.group_send)(
             self.room_group_name,
             {
-                "type": "chat.message",
+                "type": "chat_message_handler",
                 "message": message_response,
             },
         )
 
-    def chat_message(self, event):
-        self.send(json.dumps({"ok": True, "message": event["message"]}))
+    def chat_message_handler(self, event):
+        self.send(
+            json.dumps({"ok": True, "message": event["message"], "event": "message"})
+        )
 
     def get_room_name(self) -> str:
         return self.scope["url_route"]["kwargs"]["chatroom_name"]  # type: ignore
 
-    def get_chat_group(self) -> ChatGroup | None:
-        chat_group = ChatGroup.objects.filter(group_name=self.get_room_name()).first()
+    def get_chat_group(self) -> ChatGroup:
+        chat_group_name = self.get_room_name()
+        chat_group = ChatGroup.objects.filter(group_name=chat_group_name).first()
+        if not chat_group:
+            raise RuntimeError(f"Chat Group: {chat_group_name} not found")
         return chat_group
+
+    def update_online_count(self):
+        online_count = self.chat_group.users_online.count()
+        async_to_sync(self.channel_layer.group_send)(
+            self.room_group_name,
+            {
+                "type": "online_count_handler",
+                "online_count": online_count,
+            },
+        )
+
+    def online_count_handler(self, event):
+        self.send(
+            json.dumps(
+                {
+                    "ok": True,
+                    "online_count": event["online_count"],
+                    "event": "online_count",
+                }
+            )
+        )
